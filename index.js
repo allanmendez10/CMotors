a380f3ca9ccfb7dcb53e33bb8ff5b2a926c6cda2
acordero <acordero@tecnosys.net>
2020-08-17 15:05:44 -0600

    Updating json1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+
 //middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -8,7 +9,9 @@ app.use(express.urlencoded({ extended: false }))
 //routes
 app.use(require('./routes/index'))
 
+const port = process.env.PORT
 
-app.listen(3000, () => {
+
+app.listen(port, () => {
     console.log('Server is up on port ' + 3000)
 })
